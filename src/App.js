ef3d7199107515617cadd6b4ecb47853e524bad5
Alejandro <aesbetancourt@outlook.com>
2020-06-10 10:08:13 -0400

    dashboard-app added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Redirect } from 'react-router';
-import './App.css';
-import Home from './components/home'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./body.css";
+import "antd/dist/antd.css";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { Layout } from "antd";
+import cubejs from "@cubejs-client/core";
+import { CubeProvider } from "@cubejs-client/react";
+import client from "./graphql/client";
+import Header from "./components/Header";
+const API_URL = "http://localhost:5000";
+const CUBEJS_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTE3MTgzNjYsImV4cCI6MTU5MTgwNDc2Nn0.A97-l_DYw9lRxezNx2aMzhmUrQkeU5MkODEyVlw3opc";
+const cubejsApi = cubejs(CUBEJS_TOKEN, {
+  apiUrl: `${API_URL}/cubejs-api/v1`
+});
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route exact path="/home" component={Home} />
-            <Route path="*" render={() => (
-            <Redirect to="/home"/> )}/>
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const AppLayout = ({ children }) => (
+  <Layout
+    style={{
+      height: "100%"
+    }}
+  >
+    <Header />
+    <Layout.Content>{children}</Layout.Content>
+  </Layout>
+);
+
+const App = ({ children }) => (
+  <CubeProvider cubejsApi={cubejsApi}>
+    <ApolloProvider client={client}>
+      <AppLayout>{children}</AppLayout>
+    </ApolloProvider>
+  </CubeProvider>
+);
+
 export default App;
