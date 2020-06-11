a2a052fc4bce0b4ed15d4de6f67f8ce86d582f12
Alejandro <aesbetancourt@outlook.com>
2020-06-11 13:18:13 -0400

    improved dashboard

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { withRouter } from "react-router";
-import { Layout, Menu } from "antd";
+import { Layout, Menu, Button } from "antd";
+import axios from "axios";
+
+function updateDB(){
+    axios.get("http://localhost:5000/test1")
+        .then((data) => {
+            // alert(data.data)
+            console.log("DB Up to date")
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+
+}
 
 const Header = ({ location }) => (
   <Layout.Header
@@ -14,33 +27,22 @@ const Header = ({ location }) => (
         float: "left"
       }}
     >
-      <h2
-        style={{
-          color: "#fff",
-          margin: 0,
-          marginRight: "1em",
-          display: "inline",
-          width: 100,
-          lineHeight: "54px"
-        }}
-      >
-        My Dashboard
-      </h2>
+        <img src={require('../assets/logo.png')} style={{marginRight: "20px"}} alt="logo" />
     </div>
     <Menu
       theme="dark"
       mode="horizontal"
       selectedKeys={[location.pathname]}
       style={{
-        lineHeight: "64px"
-      }}
-    >
-      <Menu.Item key="/explore">
-        <Link to="/explore">Explore</Link>
+        lineHeight: "64px",
+      }}>
+      <Menu.Item key="/explore" className="customclass">
+        <Link to="/explore">Crear Reportes</Link>
       </Menu.Item>
-      <Menu.Item key="/">
+      <Menu.Item key="/" className="customclass">
         <Link to="/">Dashboard</Link>
       </Menu.Item>
+        <Button type="primary" onClick={() => updateDB()} style={{ backgroundColor: "#08979c", borderColor: "#08979c", float: "right", margin: '16px' }}>Actualizar BD</Button>
     </Menu>
   </Layout.Header>
 );
