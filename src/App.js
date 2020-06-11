17386ca962cf92d5133375a4f40750fae4bf2d09
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2020-06-11 15:33:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from "react";
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
 import "./body.css";
 import "antd/dist/antd.css";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { Layout } from "antd";
 import cubejs from "@cubejs-client/core";
-// import WebSocketTransport from '@cubejs-client/ws-transport';
 import { CubeProvider } from "@cubejs-client/react";
 import client from "./graphql/client";
 import Header from "./components/Header";
-const API_URL = "http://localhost:5000";
+import WebSocketTransport from '@cubejs-client/ws-transport';
+const API_URL = 'ws://localhost:5000/';
 const CUBEJS_TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTE3MTgzNjYsImV4cCI6MTU5MTgwNDc2Nn0.A97-l_DYw9lRxezNx2aMzhmUrQkeU5MkODEyVlw3opc";
-const cubejsApi = cubejs(CUBEJS_TOKEN, {
-  apiUrl: `${API_URL}/cubejs-api/v1`
+const cubejsApi = cubejs({
+  transport: new WebSocketTransport({ authorization: CUBEJS_TOKEN, apiUrl: API_URL })
 });
-// const cubejsApi = cubejs({
-//     transport: new WebSocketTransport({ authorization: CUBEJS_TOKEN, apiUrl: 'ws://localhost:5000/cubejs-api/v1' })
-// });
 
 const AppLayout = ({ children }) => (
   <Layout
